c82b72e4ce9502bb11afc47cc91e9e2dfd4c75d0
Oskar <pawicao@gmail.com>
2020-05-16 00:06:53 +0200

    getAll

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import json
 from flask import request, jsonify
 
 app = flask.Flask(__name__)
@@ -9,13 +10,20 @@ def home():
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	return response
 
-
 # A route to return an example "Hello!"
 @app.route('/hello', methods=['GET'])
 def api_hello():
 	response = jsonify("Elo, byku!")
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	return response
+	
+# Get all countries
+@app.route('/getAll', methods=['GET'])
+def get_all_countries():
+	with open('assets/all_countries.json', 'r') as file:
+		response = file.read()
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		return response
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
