28cede53840f2fe9f72088c93de7709d6d82e94d
Oskar <pawicao@gmail.com>
2020-05-18 21:15:51 +0200

    Testing post requests

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 app = flask.Flask(__name__)
 
 
-@app.route('/')
-def home():
-    response = jsonify("HOME PAGE!")
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-
-# A route to return an example "Hello!"
-@app.route('/hello', methods=['GET'])
-def api_hello():
-    response = jsonify("Elo, byku!")
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-
 # Get all countries
+@app.route('/', methods=['GET'])
 @app.route('/getAll', methods=['GET'])
 def get_all_countries():
     with open('assets/all_countries.json', 'r') as file:
@@ -27,6 +13,16 @@ def get_all_countries():
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.mimetype = "application/json"
         return response
+
+
+# Process answer, return filtered list of countries together with a new question
+@app.route('/processQuestion', methods=['POST'])
+def process_question():
+    data = request.json
+    response = jsonify(data)
+    response.mimetype = "application/json"
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
