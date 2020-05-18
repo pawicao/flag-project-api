c5ce23decb8a45125a8e8737be5771ae4085fe6a
Oskar <pawicao@gmail.com>
2020-05-18 23:20:35 +0200

    Corrected JSONing

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_all_countries():
 def start():
     with open('assets/all_countries.json', 'r') as file:
         countries = file.read()
-    response_content = '{ "countries": ' + countries + ',' + '"question:"' + json.dumps({
+    response_content = '{ "countries": ' + countries + ',' + '"question":' + json.dumps({
         "id": 1,
         "content": questions[1]
     })
