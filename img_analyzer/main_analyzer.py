6b54e0c6111b2deb99949f3e5edd6e0a38aef76f
Oskar <pawicao@gmail.com>
2020-05-18 22:54:55 +0200

    corrected req

--- img_analyzer/main_analyzer.py
+++ img_analyzer/main_analyzer.py
@@ -7,22 +7,22 @@ from utils.functions import nope
 # -- Color detection --
 
 # load the image
-image = cv2.imread('pol.png')
+#image = cv2.imread('pol.png')
 
 # define the list of boundaries
 #       OPENCV BGR
-red = ([20, 20, 108], [233, 233, 251])
+#red = ([20, 20, 108], [233, 233, 251])
 
 # create NumPy arrays from the boundaries
-lower = np.array(red[0], dtype="uint8")
-upper = np.array(red[1], dtype="uint8")
+#lower = np.array(red[0], dtype="uint8")
+#upper = np.array(red[1], dtype="uint8")
 # find the colors within the specified boundaries and apply
 # the mask
-mask = cv2.inRange(image, lower, upper)
-output = cv2.bitwise_and(image, image, mask=mask)
+#mask = cv2.inRange(image, lower, upper)
+#output = cv2.bitwise_and(image, image, mask=mask)
 # show the images
-cv2.imshow("images", np.hstack([image, output]))
-cv2.waitKey(0)
+#cv2.imshow("images", np.hstack([image, output]))
+#cv2.waitKey(0)
 
 
 # --
